91b2c6b45a72fd8e29fe224eba01d037cd8e7fb7
Claudiu <sprancenatuc@yahoo.com>
2020-05-31 17:05:47 +0300

    Login2 with valid credentials commit

--- src/main/java/pages/PageObjectManager.java
+++ src/main/java/pages/PageObjectManager.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 public class PageObjectManager {
 
     private BasePage basePage;
+    private ShoppingCartPage shoppingCartPage;
+    private ProductPage productPage;
+    private ShippingPage shippingPage;
 
     public PageObjectManager(WebDriver driver){
         basePage = new BasePage(driver);
