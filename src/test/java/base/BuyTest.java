cf342015363286594fb3a5727f1f6a86082786f2
Claudiu <sprancenatuc@yahoo.com>
2020-06-14 14:52:37 +0300

    BuyTest added on this commit

---
+++ src/test/java/base/BuyTest.java
@@ -0,0 +1,38 @@
+package base;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.ProductPage;
+import pages.ShippingPage;
+import pages.ShoppingCartPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class BuyTest extends  BaseTests {
+
+    private HomePage homePage;
+    private ProductPage productPage;
+    private ShoppingCartPage shoppingCartPage;
+    private ShippingPage shippingPage;
+
+    @BeforeMethod
+    public void setPages(){
+        productPage = pages.getProductPage();
+        shoppingCartPage = pages.getShoppingCartPage();
+        shippingPage = pages.getShippingPage();
+        homePage = pages.getHomePage();
+    }
+
+
+    @Test
+    public void validateLoginInBuingProcess(){
+        homePage.open().SearchProduct("Marco Lightweight Active Hoodie");
+        productPage.selectSizeAndColor();
+        productPage.clickOnShoppingCart();
+        shoppingCartPage.clickToCheckout();
+        shippingPage.clickToSignInLink();
+        shippingPage.SignIn("roni_cost@example.com", "roni_cost3@example.com");
+        //assertTrue(shippingPage.checkFirstname().contains("Veronica"), "The text is incorrect!");
+    }
+}
